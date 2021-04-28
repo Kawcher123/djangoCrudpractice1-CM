3df4314cca09bc7e872298300da11b4a755dc100
Kawcher123 <kawcherahmed43@gmail.com>
2021-04-29 03:21:49 +0600

    customer wise order

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='home'),
+    path('login/',views.login,name='login'),
+    path('signup/',views.signup,name='signup'),
     path('products/', views.products, name='products'),
     path('customer/<str:pk>/',views.customer,name='customer'),
     path('add_product/',views.add_product,name='add_product'),
-    path('create_order/',views.create_order,name='create_order'),
+    path('create_order/<str:pk>/',views.create_order,name='create_order'),
+    path('update_order/<str:pk>/',views.update_order,name='update_order'),
+    path('delete_order/<str:pk>/',views.delete_order,name='delete_order')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,24 @@ from django.shortcuts import render,redirect
 from .models import *
 
 from .forms import *
+from django.forms import inlineformset_factory
+
+from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+def signup(request):
+    form=UserCreationForm()
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context={'form':form}
+    return render(request,'signup.html',context)
+
+def login(request):
+    context={}
+    return render(request,'login.html',context)
+
 
 def home(request):
     orders=Order.objects.all()
@@ -29,20 +46,46 @@ def customer(request,pk):
     customer=Customer.objects.get(id=pk)
     orders = customer.order_set.all()
     order_count = orders.count()
+    myfilter=OrderFilter(request.GET,queryset=orders)
+    orders=myfilter.qs
 
-    context = {'customer':customer, 'orders':orders, 'order_count':order_count}
+    context = {'customer':customer, 'orders':orders, 'order_count':order_count,'myfilters':myfilter}
     return render(request,'customer.html',context)
 
 def add_product(request):
     context={}
     return render(request,'add_product.html',context)
 
-def create_order(request):
-    form=OrderForm()
+def create_order(request,pk):
+    OrderFormSet=inlineformset_factory(Customer,Order,fields=('product','status'),extra=10)
+    customer=Customer.objects.get(id=pk)
+    formset=OrderFormSet(queryset=Order.objects.none(),instance=customer)
+    #form=OrderForm(initial={'customer':customer})
     if request.method=='POST':
-        form=OrderForm(request.POST)
+        formset=OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid:
+            formset.save()
+            return redirect('/')
+    context={'formset':formset}
+    return render(request,'create_order.html',context)
+
+def update_order(request,pk):
+    order=Order.objects.get(id=pk)
+    form=OrderForm(instance=order)
+    if request.method=='POST':
+        form=OrderForm(request.POST,instance=order)
         if form.is_valid:
             form.save()
             return redirect('/')
     context={'form':form}
     return render(request,'create_order.html',context)
+
+
+def delete_order(request,pk):
+    order=Order.objects.get(id=pk)
+    if request.method=='POST':
+        order.delete()
+        return redirect('/')
+
+    context={'order':order}
+    return render(request,'delete_order.html',context)
